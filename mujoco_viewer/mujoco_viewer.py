3ffac15474a97bc5568436389f9b3b4180b0cf56
Mrunal Sarvaiya <mrunaljsarvaiya@gmail.com>
2024-09-18 14:39:25 -0400

    remove deprecated args, add clear_markers flag

--- mujoco_viewer/mujoco_viewer.py
+++ mujoco_viewer/mujoco_viewer.py
@@ -194,10 +194,6 @@ class MujocoViewer(Callbacks):
         g.objtype = mujoco.mjtObj.mjOBJ_UNKNOWN
         g.objid = -1
         g.category = mujoco.mjtCatBit.mjCAT_DECOR
-        g.texid = -1
-        g.texuniform = 0
-        g.texrepeat[0] = 1
-        g.texrepeat[1] = 1
         g.emission = 0
         g.specular = 0.5
         g.shininess = 0.5
@@ -377,7 +373,7 @@ class MujocoViewer(Callbacks):
             mujoco.mjr_readPixels(img, None, self.viewport, self.ctx)
             return np.flipud(img)
 
-    def render(self):
+    def render(self, clear_markers=True):
         if self.render_mode == 'offscreen':
             raise NotImplementedError(
                 "Use 'read_pixels()' for 'offscreen' mode.")
@@ -468,7 +464,8 @@ class MujocoViewer(Callbacks):
                 self._loop_count -= 1
 
         # clear markers
-        self._markers[:] = []
+        if clear_markers:
+            self._markers[:] = []
 
         # apply perturbation (should this come before mj_step?)
         self.apply_perturbations()
